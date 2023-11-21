f20c250a8683292dff4695f45d826024ecc40f3f
yoshida <c0a22149bf@edu.teu.ac.jp>
2023-11-21 15:09:37 +0900

    ２問目

--- pygame_image.py
+++ pygame_image.py
@@ -4,15 +4,18 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kouk_img = pg.image.load("ex01/fig/3.png")
+    kouk_img_gyaku = pg.transform.flip(kouk_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kouk_img_gyaku, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
